6d2fd3f5c3d2695f2bbf0e74b18842d4b984f38a
himanshi-kumar <himanshikumar73@gmail.com>
2026-08-04 19:38:15 +0530

    Time: 0 ms (100%), Space: 46.7 MB (99.58%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,26 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+
+        long pow = n;
+
+        if (pow < 0) {
+            x = 1 / x;
+            pow = -pow;
+        }
+
+        return solved(x, pow);
+    }
+
+    public double solved(double x, long n) {
+
+        if (n == 0)
+            return 1;
+
+        double half = solved(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+
+        return x * half * half;
     }
 }
